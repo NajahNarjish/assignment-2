4fdeddaed82fe53a31a0c828bafadf56eb84adcf
Syeda Najah Narjish <najah.masum@gmail.com>
2024-05-29 12:28:58 +0200

    created product.interface

---
+++ src/app/moduels/product/product.model.ts
@@ -0,0 +1,26 @@
+import { Schema, model, connect } from 'mongoose';
+import { Inventory, Product, Variant } from './product.interface';
+
+const variantSchema = new Schema<Variant>({
+  type: String,
+  value: String,
+});
+const inventorySchema = new Schema<Inventory>({
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    required: true,
+  },
+});
+const productSchema = new Schema<Product>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  tags: [String],
+  variants: [variantSchema],
+  inventory: inventorySchema,
+});
