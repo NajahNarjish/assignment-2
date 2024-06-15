8b9594e4a62cf0dda7ede36d4651075268232f20
Syeda Najah Narjish <najah.masum@gmail.com>
2024-06-15 14:43:17 +0200

    [Done] create product feature

--- src/app/moduels/product/product.controller.ts
+++ src/app/moduels/product/product.controller.ts
@@ -16,6 +16,36 @@ const createProduct = async (req: Request, res: Response) => {
   }
 };
 
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProductsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: 'Products are retrieved successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const { productId } = req.params;
+    const result = await ProductServices.getSingleProductFromDB(productId);
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrieved successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const ProductControllers = {
   createProduct,
+  getAllProducts,
+  getSingleProduct,
 };
--- src/app/moduels/product/product.service.ts
+++ src/app/moduels/product/product.service.ts
@@ -5,6 +5,16 @@ const createProductIntoDB = async (product: Product) => {
   const result = await ProductModel.create(product);
   return result;
 };
+const getAllProductsFromDB = async () => {
+  const result = await ProductModel.find();
+  return result;
+};
+const getSingleProductFromDB = async (_id: string) => {
+  const result = await ProductModel.findOne({ _id });
+  return result;
+};
 export const ProductServices = {
   createProductIntoDB,
+  getAllProductsFromDB,
+  getSingleProductFromDB,
 };
